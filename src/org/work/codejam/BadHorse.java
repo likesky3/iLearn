6c337bfc97546fcbd210010e1bdfe01dec89ba73
Lynn <Lynn@Lynn-PC>
2013-09-15 21:39:24 +0800

    update

--- src/org/work/codejam/BadHorse.java
+++ src/org/work/codejam/BadHorse.java
@@ -6,6 +6,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class BadHorse {
 
@@ -18,68 +20,77 @@ public class BadHorse {
 		PrintWriter out = new PrintWriter(new FileWriter("a.out"));
 
 		int N = Integer.parseInt(in.readLine());
-		int k = 0;
+		int k = 1;
 
-		int num = 0;
-		String[] pair = new String[2];
-		HashMap<String, Integer> group1 = new HashMap<String, Integer>();
-		HashMap<String, Integer> group2 = new HashMap<String, Integer>();
-		while (k < N) {
-			group1.clear();
-			group2.clear();
-			k++;
-			
-			String line = in.readLine();
-//			System.out.println(line);
-			num = Integer.parseInt( line);
-			if (num == 1) {
-				in.readLine();
+		int c;
+		HashMap<String, HashSet<String>> graph = new HashMap<String, HashSet<String>>();
+		HashMap<String, Integer> colormap = new HashMap<String, Integer>();
+		LinkedList<String> queue = new LinkedList<String>();
+		String[] points;
+		boolean flag = true;
+		while(k <= N){
+			c = Integer.parseInt(in.readLine());
+			if(c == 1){
 				out.println("Case #" + k + ": " + "Yes");
+				in.readLine();
+				k++;
 				continue;
 			}
-			int i = 0;
-			for (; i < num; i++) {
-				line = in.readLine();
-				pair = line.split(" ");
-//				pair = in.readLine().split(" ");
-				if (k == 1) {
-					group1.put(pair[0], 1);
-					group2.put(pair[1], 1);
-					continue;
-				}
-				if (!group2.containsKey(pair[0])){
-					//p0 -> group1
-					group1.put(pair[0], 1);
-					if(!group1.containsKey(pair[1]))
-						group2.put(pair[1], 1);
-					else{
-						out.println("Case #" + k + ": " + "No");
-						break;
-					}
-				}else{
-					if(!group2.containsKey(pair[1]))
-						group1.put(pair[1], 1);
-					else{
-						out.println("Case #" + k + ": " + "No");
-						break;
-					}
-						
-				}
-
-			}
-			if(i == num)
-				out.println("Case #" + k + ": " + "Yes");
-			i++;
-			while(i < num){
-				line = in.readLine();
-//				System.out.println(line);
-				i++;
-			}
-//			System.out.println("---");
 			
+			//1 build graph
+			graph.clear();
+			for(int i = 0; i < c; i++){
+				points = in.readLine().split(" ");
+				buildGraph(graph, points[0], points[1]);
+				buildGraph(graph, points[1], points[0]);
+			}
+			
+			//2 verify if the graph is a bipartite graph, use BFS, use queue, mark color then enqueue
+			 colormap.clear();
+			 queue.clear();
+			 flag = true;
+			 
+			 String startNode = graph.keySet().toArray(new String[1])[0];
+			 colormap.put(startNode, 0);
+			 queue.offer(startNode);
+			 while(!queue.isEmpty()){
+				 String v1 = queue.poll();
+				 int ucolor = colormap.get(v1) == 0 ? 1 : 0;
+				 
+				 //for each neighbor node, mark color and enqueue
+				 for(String v2: graph.get(v1)){
+					 if(!colormap.containsKey(v2)){
+						 colormap.put(v2, ucolor);
+						 //only enqueue not processed node
+						 queue.offer(v2);
+					 }else if(colormap.get(v2) != ucolor){//conflict
+						 flag = false;
+						 break;
+					 }
+				 }
+				 if(!flag)
+					 break;
+			 }
+			 if(flag)
+				 out.println("Case #" + k + ": " + "Yes");
+			 else
+				 out.println("Case #" + k + ": " + "No");
+			 
+			k++;
 		}
+		
 		in.close();
 		out.close();
+	}
+	
+	private static void buildGraph(HashMap<String, HashSet<String>> graph, String v1, String v2){
+		if(!graph.containsKey(v1)){
+			HashSet<String> neighs = new HashSet<String>();
+			neighs.add(v2);
+			graph.put(v1, neighs);
+		}else{
+			graph.get(v1).add(v2);
+		}
 	}
 
 }
