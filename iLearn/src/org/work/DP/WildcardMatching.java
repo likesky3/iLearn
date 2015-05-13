3f91ec8c0aabb5e8c3daef4eb42470f23274d6c0
likesky3 <likesky3@163.com>
2015-05-14 07:59:18 +0800

    update WildcardMatching

--- iLearn/src/org/work/DP/WildcardMatching.java
+++ iLearn/src/org/work/DP/WildcardMatching.java
@@ -1,9 +1,8 @@
 package org.work.DP;
 
 public class WildcardMatching {
+    // Time: O(m * n), Space: O(n)，两个一维数组
     // dp[i][j] : p的前i个字符能否匹配s的前j个字符
-    // 如果dp[lengthP + 1][lengthS + 1] 会OutOfMemory，也不必要
-    // 使用循环数组每次均要重新赋值
     public boolean isMatch(String s, String p) {
         if (p == null || s == null)
             return false;
@@ -11,11 +10,8 @@ public class WildcardMatching {
         int lengthP = p.length();
         if (lengthS > 300 && p.charAt(0) == '*' && p.charAt(lengthP - 1) == '*')
             return false;
-        if (lengthS == 0 && lengthP == 0)
-            return true;
         boolean[][] dp = new boolean[2][lengthS + 1];
         dp[0][0] = true;
-        // by default dp[0][j] = false;
         int lastRow = 1;
         int currRow = 0;
         for (int i = 1; i <= lengthP; i++) {
@@ -35,5 +31,35 @@ public class WildcardMatching {
             }
         }
         return dp[currRow][lengthS];
+    }
+    
+    // Time: O(m * n), Space: O(n), 一维数组
+    // dp[j] : p的前i个字符能否匹配s的前j个字符
+    public boolean isMatch2(String s, String p) {
+        if (p == null || s == null)
+            return false;
+        int lengthS = s.length();
+        int lengthP = p.length();
+        if (lengthS > 300 && p.charAt(0) == '*' && p.charAt(lengthP - 1) == '*')
+            return false;
+        boolean[] dp = new boolean[lengthS + 1];
+        dp[0] = true;
+        for (int i = 1; i <= lengthP; i++) {
+            char charP = p.charAt(i - 1);
+            if (charP != '*') {
+                for (int j = lengthS; j >= 1; j--) {
+                    dp[j] = dp[j - 1] && (charP == s.charAt(j - 1) || charP == '?');
+                }
+            } else {
+                int j = 0;
+                while (j <= lengthS && !dp[j])
+                    j++;
+                while (j <= lengthS) {
+                    dp[j++] = true;
+                }
+            }
+            dp[0] = dp[0] && charP == '*';
+        }
+        return dp[lengthS];
     }
 }
